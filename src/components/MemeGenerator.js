cd4f213c0c3f4c972d03ae5417bea3ffd8b354f8
adityabisoi <adityabisoi1999@gmail.com>
2020-02-26 21:36:48 +0530

    image generator adde

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends React.Component{
             allMemeImgs:[]
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
     componentDidMount(){
         fetch("https://api.imgflip.com/get_memes")
@@ -23,14 +24,25 @@ class MemeGenerator extends React.Component{
         const {name, value} = event.target
         this.setState({[name] : value})
     }
+    handleSubmit(event){
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+        this.setState({randomImage : randMemeImg})
+    }
     render(){
         return(
             <div>
-                <form className="meme-form">
+                <form className="meme-form" onSubmit={this.handleSubmit}>
                     <input type="text" name="topText" value={this.state.topText} placeholder="Top Text" onChange={this.handleChange}/>
                     <input type="text" name="bottomText" value={this.state.bottomText} placeholder="Bottom Text" onChange={this.handleChange}/>
                     <button>Generate</button>
                 </form>
+                <div className="meme">
+                    <img src={this.state.randomImage} alt=""/>
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </div>
         );
     }
