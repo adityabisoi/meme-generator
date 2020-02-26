2cb4834b16e34a9a95a3d11a39c8950d813044cc
adityabisoi <adityabisoi1999@gmail.com>
2020-02-26 20:49:02 +0530

    input form added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <Header/>
-      <h1>Hello World</h1>
       <MemeGenerator/>
     </div>
   );
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -4,12 +4,34 @@ class MemeGenerator extends React.Component{
     constructor(){
         super()
         this.state = {
-            
+            topText:"",
+            bottomText:"",
+            randomImage:"http://i.imgflip.com/1bij.jpg",
+            allMemeImgs:[]
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response =>{
+                const {memes} = response.data
+                this.setState({allMemeImgs:memes})
+            })
+    }
+    handleChange(event){
+        const {name, value} = event.target
+        this.setState({[name] : value})
     }
     render(){
         return(
-            <h1>Meme Generator</h1>
+            <div>
+                <form className="meme-form">
+                    <input type="text" name="topText" value={this.state.topText} placeholder="Top Text" onChange={this.handleChange}/>
+                    <input type="text" name="bottomText" value={this.state.bottomText} placeholder="Bottom Text" onChange={this.handleChange}/>
+                    <button>Generate</button>
+                </form>
+            </div>
         );
     }
 }
