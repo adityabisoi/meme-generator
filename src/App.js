2f547a18ad9b893325d388959da142bd3529529f
adityabisoi <adityabisoi1999@gmail.com>
2020-02-26 20:27:02 +0530

    component files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import MemeGenerator from './components/MemeGenerator'
 
 function App() {
   return (
     <div>
+      <Header/>
       <h1>Hello World</h1>
+      <MemeGenerator/>
     </div>
   );
 }
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+    render(){
+        return(
+            <h1>Meme Generator</h1>
+        );
+    }
+}
+
+export default MemeGenerator
